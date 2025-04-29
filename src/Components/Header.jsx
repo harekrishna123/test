5af4654a5bbbfaab7bfd96bab91c3187ba8333d0
Hare Krishna <hare.krishna@prudencesoftech.in>
2025-04-29 15:29:17 +0530

    change hi header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,11 +11,7 @@ function Header({isAuthenticated}) {
         <Navbar.Brand href="#home">Ctax </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-           
-          </Nav>
+         
           <Nav className="me-auto text-right">
           {isAuthenticated?( 
             <>
